730862f68bf098603530d795f2bf9d1e74a2ad5d
Ricardo Morato <36631@thoughtworks.com>
2023-01-24 12:18:05 -0300

    chore: initialize project

---
+++ ricardoflix/src/main/java/dev/ricardomorato/ricardoflix/RicardoflixApplication.java
@@ -0,0 +1,13 @@
+package dev.ricardomorato.ricardoflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicardoflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicardoflixApplication.class, args);
+	}
+
+}
---
+++ ricardoflix/src/test/java/dev/ricardomorato/ricardoflix/RicardoflixApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ricardomorato.ricardoflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicardoflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
